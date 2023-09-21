ce2058fabab0cc37a46e6611122d056a716e44b4
Shabnamdeep Kaur <122659933+ShabnamdeepKaur@users.noreply.github.com>
2023-09-20 23:54:07 -0400

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using HomeHelpHub.Models;
+
+namespace HomeHelpHub.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public IActionResult Brief()
+    {
+        List<Dictionary<string, object>> info =new(){
+                new() { {"Company", "HomeHelpHub.com"}, 
+                {"Company-Description", "Worker-Needy connection Hub"}, 
+                {"Group-Members-And-Roles", "Shabnamdeep Kaur- the wholesole developer of this website."}  
+                },
+            };
+
+            return View(info);
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
